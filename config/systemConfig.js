86ce278170f559e7102cc32df7a1bd7dc196b2c2
jeffersonfbb1997 <jeffersonfbb1997@gmail.com>
2026-03-19 10:25:57 -0300

    Delete config directory

--- config/systemConfig.js
+++
@@ -1,6 +0,0 @@
-export const COMPANIES = {
-    atlantico: { name: "Atlântico", favicon: "images/atlantico-favicon.png" },
-    viametro: { name: "Via Metro", favicon: "images/viametro-favicon.png" }
-};
-
-export const ADMIN_EMAIL = "jeffersonfbb1997@gmail.com";
