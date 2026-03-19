437703ab78bf1b6c9910219c5c99a51e9324d5f7
jeffersonfbb1997 <jeffersonfbb1997@gmail.com>
2026-03-19 10:28:05 -0300

    Delete utils directory

--- utils/geoUtils.js
+++
@@ -1,7 +0,0 @@
-export const calcDist = (la1, lo1, la2, lo2) => {
-    const R = 6371;
-    const dLat = (la2-la1)*Math.PI/180; 
-    const dLon = (lo2-lo1)*Math.PI/180;
-    const a = Math.sin(dLat/2)**2 + Math.cos(la1*Math.PI/180)*Math.cos(la2*Math.PI/180)*Math.sin(dLon/2)**2;
-    return R * 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-};
